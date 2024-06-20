95cd69bc461f09b3c3fbec3446bdf85f4059f911
Allaezhya <38576460+Allaezhya@users.noreply.github.com>
2024-06-20 10:29:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ import pickle
 import pandas as pd
 import os
 from PIL import Image
+import requests
 
 def recommend(kerajinan):
     index = crochet[crochet['kerajinan'] == kerajinan].index[0]
     crochet_list = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_crochet = []
+    base_url = 'https://raw.githubusercontent.com/Allaezhya/Crochet-Recommended-System/repository/utama/images'
+    
     for i in crochet_list:
         kerajinan_name = crochet.iloc[i[0]].kerajinan
         link = crochet.iloc[i[0]].link
         image_jalur = crochet.iloc[i[0]].gambar
 
-        full_image_path = os.path.join('https://raw.githubusercontent.com/Allaezhya/Crochet-Recommended-System/repository/utama/images', image_jalur)
+        full_image_path = base_url + image_jalur
 
         recommended_crochet.append([kerajinan_name, link, full_image_path])
     return recommended_crochet
@@ -39,9 +42,9 @@ if st.button("Recommend"):
         st.subheader(kerajinan_name)
         st.write(f"[Link ke tutorial] ({link})")
 
-        st.write(f"Path gambar: {gambar}")  # Debugging untuk memeriksa path gambar
-        if os.path.exists(gambar):
-            image = Image.open(gambar)
+        st.write(f"URL gambar: {gambar}")  # Debugging untuk memeriksa path gambar
+        try:
+            image = Image.open(requests.get(gambar, stream=True).raw)
             st.image(image, caption=kerajinan_name, use_column_width=True)
-        else:
+        except Exception as e:
             st.write("Gambar tidak ditemukan!")

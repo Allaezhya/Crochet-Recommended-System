c650be7e57177653a9af4f73a4051977e52f60cf
Allaezhya <38576460+Allaezhya@users.noreply.github.com>
2024-06-29 09:05:57 +0700

    Perubahan beberapa hal

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import os
 from PIL import Image
 import requests
+import io
 
 def recommend(kerajinan):
     index = crochet[crochet['kerajinan'] == kerajinan].index[0]
     crochet_list = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_crochet = []
-    base_url = 'https://github.com/Allaezhya/Crochet-Recommended-System/tree/utama/images/'
+    base_url = 'https://drive.google.com/drive/folders/1lfrU2AUAnycRQofAAO-OtX0D9-alw_Yb?usp=drive_link'
     for i in crochet_list:
         kerajinan_name = crochet.iloc[i[0]].kerajinan
         link = crochet.iloc[i[0]].link
@@ -26,24 +26,27 @@ crochet = pd.DataFrame(crochet_dict)
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-st.title("Crochet Recommender System")
+st.title("Sistem Rekomendasi Kerajinan Rajut")
 
 selected_crochet = st.selectbox(
-'Kerajinan apa yang ingin ada cari sebagai basis rekomendasi?',
+'Mencri projek selanjutnya? Pilih salah satu',
 crochet['kerajinan'].values)
 
-if st.button("Recommend"):
+if st.button("Rekomendasikan"):
     recommendations = recommend(selected_crochet)
-
     st.write(f"Rekomendasi untuk {selected_crochet}:")
+
     for rec in recommendations:
         kerajinan_name, link, gambar = rec
         st.subheader(kerajinan_name)
         st.write(f"[Link ke tutorial] ({link})")
+
+        st.write(f"URL gambar: {gambar}")  # Debugging untuk memeriksa URL gambar
         try:
             response = requests.get(gambar, stream=True)
             if response.status_code == 200:
-                image = Image.open(response.raw)
+                image_bytes = io.BytesIO(response.content)
+                image = Image.open(image_bytes)
                 st.image(image, caption=kerajinan_name, use_column_width=True)
             else:
                 st.write("Gambar tidak ditemukan!")

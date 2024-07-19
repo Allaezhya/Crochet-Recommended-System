8f5344d9fff0efbb3f33e9f498cc53ba43aaef41
Allaezhya <Allaehya@gmail.com>
2024-07-19 12:05:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ if st.button("Rekomendasikan"):
         st.write(f"[Link ke tutorial] ({link})")
 
         st.write(f"URL gambar: {gambar}")  # Debugging untuk memeriksa URL gambar
-        with st.echo():
-            st.markdown("[![Click me](./app/static/alpukat.png)](https://streamlit.io)")
+        st.markdown("[![Click me](./app/static/alpukat.png)](https://streamlit.io)")
         try:
             response = requests.get(gambar, stream=True)
             if response.status_code == 200:

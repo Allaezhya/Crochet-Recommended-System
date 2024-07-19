3e7e661d00f3b61a71eef2a055e2344e166b7e58
Allaezhya <Allaehya@gmail.com>
2024-07-19 11:14:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if st.button("Rekomendasikan"):
         st.write(f"[Link ke tutorial] ({link})")
 
         st.write(f"URL gambar: {gambar}")  # Debugging untuk memeriksa URL gambar
-        st.markdown(f"[![Click me](./static/{gambar})]({link})")
+        st.markdown(f"[![Click me](./static/apel.png)]({link})")
         try:
             response = requests.get(gambar, stream=True)
             if response.status_code == 200:
